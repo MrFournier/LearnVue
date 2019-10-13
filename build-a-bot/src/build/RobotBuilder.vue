f042500b91d89b20abe5dbb15a9dd2390e19418b
MrFournier <troy_fournier@live.com>
2019-10-13 12:57:50 -0700

    Completed module 2

--- build-a-bot/src/build/RobotBuilder.vue
+++ build-a-bot/src/build/RobotBuilder.vue
@@ -2,7 +2,7 @@
   <div class="content">
     <button class="add-to-cart" @click="addToCart()">Add to Cart</button>
     <div class="top-row">
-      <div class="top part">
+      <div :class="[saleBorderClass, 'top', 'part']">
         <div class="robot-name">
             {{selectedRobot.head.title}}
             <span v-if="selectedRobot.head.onSale" class="sale">Sale!</span>
@@ -66,6 +66,7 @@
 
 <script>
 import availableParts from '../data/parts';
+import createdHookMixin from './created-hook-mixin';
 
 function getPreviousValidIndex(index, length) {
   const deprecatedIndex = index - 1;
@@ -90,7 +91,13 @@ export default {
       selectedBaseIndex: 0,
     };
   },
+  mixins: [createdHookMixin],
   computed: {
+    saleBorderClass() {
+      return this.selectedRobot.head.onSale
+        ? 'sale-border'
+        : '';
+    },
     selectedRobot() {
       return {
         head: availableParts.heads[this.selectedHeadIndex],
@@ -175,15 +182,17 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss" scoped>
 .part {
   position: relative;
   width:165px;
   height:165px;
   border: 3px solid #aaa;
 }
-.part img {
-  width:165px;
+.part {
+    img {
+        width:165px;
+    }
 }
 .top-row {
   display:flex;
@@ -290,5 +299,8 @@ td, th {
 }
 .cost {
     text-align: right;
+}
+.sale-border {
+    border: 3px solid red;
 }
 </style>
