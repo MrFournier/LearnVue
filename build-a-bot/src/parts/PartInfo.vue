96956f0f9bf74d77b6b21f39824d0e196014218e
MrFournier <troy_fournier@live.com>
2019-10-14 23:13:06 -0700

    On Module 5 learning routes and params

--- build-a-bot/src/parts/PartInfo.vue
+++ build-a-bot/src/parts/PartInfo.vue
@@ -14,9 +14,18 @@ import parts from '../data/parts';
 
 export default {
   name: 'Part Info',
+  props: {
+    partType: { type: String },
+    id: {
+      type: [Number, String],
+      validator(value) {
+        return Number.isInteger(Number(value));
+      },
+    },
+  },
   computed: {
     part() {
-      const { partType, id } = this.$route.params;
+      const { partType, id } = this;
       return parts[partType].find(part => part.id === +id);
     },
   },
