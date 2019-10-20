bb34122b4f8a2b20ffb47e960e87b0133494c8a6
MrFournier <troy_fournier@live.com>
2019-10-20 14:09:47 -0700

    Completed module 7

---
+++ build-a-bot/src/shared/pin-directive.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-param-reassign */
+function applyStyle(element, binding) {
+  Object.keys(binding.value).forEach((position) => {
+    element.style[position] = binding.value[position];
+  });
+  element.style.position = 'absolute';
+}
+
+export default {
+  bind: (element, binding) => {
+    applyStyle(element, binding);
+  },
+  update: (element, binding) => {
+    applyStyle(element, binding);
+  },
+};
